9224299be4a86952d5cd8f95a6cda3e275f81531
nocka <kristi.nocka@sisal.al>
2023-04-28 16:49:55 +0200

    remove constants

--- src/main/java/com/hostfullly/bookingservice/Constants/OperationConstants.java
+++
@@ -1,8 +0,0 @@
-package com.hostfullly.bookingservice.Constants;
-
-public class OperationConstants {
-    public static final String CREATE = "Create";
-    public static final String UPDATE = "Update";
-    public static final String DELETE = "Delete";
-    public static final String CANCEL = "Cancel";
-}
